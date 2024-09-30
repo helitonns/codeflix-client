5f4979c40de40b6423f3faef3b149a82ae740889
helitonns <helitonns@gmail.com>
2024-09-30 16:03:59 -0400

    Primeiro teste.

---
+++ __tests__/hooks/useScroll.teste.jsx
@@ -0,0 +1,24 @@
+import  "@testing-library/jest-dom";
+import useScroll from '@/app/hooks/useScroll';
+import { renderHook, act } from '@testing-library/react';
+
+describe("useScroll", ()=> {
+    it("Should responde to scroll events", ()=> {
+        const {result} = renderHook(()=> useScroll());
+
+        act(()=> {
+            global.window.scrollY = 100;
+            global.window.dispatchEvent(new Event("scroll"));
+
+        });
+
+        expect(result.current).toBe(true);
+
+        act(()=> {
+            global.window.scrollY = 0;
+            global.window.dispatchEvent(new Event("scroll"));
+        });
+
+        expect(result.current).toBe(false);
+    });
+});
