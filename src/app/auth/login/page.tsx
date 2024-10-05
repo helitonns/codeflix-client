b694a732b36f6a0b489f9d701cba8b275683f217
helitonns <helitonns@gmail.com>
2024-10-05 14:24:21 -0400

    Page Login 1

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Login(){
+    return (
+        <div 
+            style={{backgroundImage: `url(/background.jpg)`}}
+            className="bg-cover bg-center bg-no-repeat text-white">
+            <div className="flex flex-col min-h-screen items-center justify-center py-2">
+                Pagina de Login
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,15 @@ import MovieRow from './components/MovieRow';
 export default function Home() {
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b from-transparent to-black lg:h-[140vh]'>
-      
       <Header />
 
       <main className='relative pb-24 pl-4 lg:pl-16'>
-
         <Banner />
-
         <MovieRow sectionTitle='Trending Now' />
         <MovieRow sectionTitle='Top Rated' />
         <MovieRow sectionTitle='Action Movies' />
-
       </main>
+      
     </div>
   );
 }
